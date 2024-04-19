1c159ff7a13588b8bbeb75c2147afa3af3f49195
Ueno02012 <k023g0064@g.neec.ac.jp>
2024-04-19 15:53:25 +0900

    ファイル全部読みなおした

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "Audio.h"
-#include <DebugCamera.h>
 #include "DirectXCommon.h"
 #include "Input.h"
 #include "Model.h"
@@ -44,26 +43,6 @@ private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
-	uint32_t textureHandle_ = 0;
-	Sprite* sprite_ = nullptr;
-
-	// ワールドトランスフォーム
-	WorldTransform worldTransform_;
-	// ビュープロジェクション
-	ViewProjection viewProjection_;
-	// 3Dモデル
-	Model* model_ = nullptr;
-
-	// サウンドデータハンドル
-	uint32_t soundDataHandle_ = 0;
-	// 音声再生ハンドル
-	uint32_t voiceHandle_ = 0;
-
-	// ImGuiで値を入力する変数
-	float inputFloat3[3] = {0, 0, 0};
-
-	// デバッグカメラ
-	DebugCamera* debugCamera_ = nullptr;
 
 	/// <summary>
 	/// ゲームシーン用
