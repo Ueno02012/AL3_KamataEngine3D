1b4ca5f37e3c12247250df46b89114ad4313fc64
Ueno02012 <k023g0064@g.neec.ac.jp>
2024-06-05 16:25:50 +0900

    test

--- PlayerBullet.cpp
+++ PlayerBullet.cpp
@@ -1,1 +1,12 @@
 #include "PlayerBullet.h"
+#include <cassert>
+#include "TextureManager.h"
+void PlayerBullet::Initialize(Model* model, const Vector3& position) {
+	
+	//NULLポインタチェック
+
+}
+
+void PlayerBullet::Update() {}
+
+void PlayerBullet::Draw(const ViewProjection& viewProjection) {}
--- PlayerBullet.h
+++ PlayerBullet.h
@@ -1,13 +1,33 @@
 #pragma once
 #include "Model.h"
 #include "Vector3.h"
+#include "WorldTransform.h"
 class PlayerBullet {
 
 public:
 
-	///<parm name = "model">モデル</param>
-	///<parm name = "position">初期座標</parm>
-	void Initialize(Model)
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model">モデル</param>
+	/// <param name="position">初期座標</param>
+	void Initialize(Model* model, const Vector3& position);
+
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	/// <param name="viewProjection">ビュープロジェクション</param>
+	void Draw(const ViewProjection& viewProjection);
+
+private:
+
+	WorldTransform worldTransform_;
+
+	Model* model_ = nullptr;
+
+	uint32_t textureHandle_ = 0u;
 
 
 };
