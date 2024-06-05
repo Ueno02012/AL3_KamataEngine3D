2147829eff83e149f803d03f3f5cf5338004fe3c
Ueno02012 <k023g0064@g.neec.ac.jp>
2024-06-05 16:38:21 +0900

    test

--- PlayerBullet.cpp
+++ PlayerBullet.cpp
@@ -4,7 +4,16 @@
 void PlayerBullet::Initialize(Model* model, const Vector3& position) {
 	
 	//NULLポインタチェック
+	assert(model);
+	model_ = model;
 
+	//テクスチャ読み込み
+	textureHandle_ = TextureManager::Load("black.png");
+
+	worldTransform_.Initialize();
+
+	//引数で受け取った初期座標をセット
+	worldTransform_.translation_ = position;
 }
 
 void PlayerBullet::Update() {}
