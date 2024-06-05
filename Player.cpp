c7e853d0a7abc0fafd3e7ee14bbf45dd64ee0c8a
Ueno02012 <k023g0064@g.neec.ac.jp>
2024-06-05 12:45:24 +0900

    test

--- Player.cpp
+++ Player.cpp
@@ -4,6 +4,7 @@ void Player::Initialize(Model* model, uint32_t textureHandle) {
 	assert(model);
 	// 引数として受け取ったデータをメンバ変数に記録
 	model_ = model;
+	textureHandle_ = textureHandle;
 	// ワールド変換の初期化
 	worldTransform_.Initialize();
 }
