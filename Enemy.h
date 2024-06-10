26d7da89223523ffbe6c4b2fa7846bf93e7c9132
Ueno02012 <k023g0064@g.neec.ac.jp>
2024-06-10 17:15:13 +0900

    test

--- Enemy.h
+++ Enemy.h
@@ -3,6 +3,13 @@
 #include "WorldTransform.h"
 #include "Vector3.h"
 class Enemy {
+
+	//行動フェーズ
+	enum class Phase {
+		Approach,//接近
+		Leave//離脱
+	};
+
 public:
 	void Initialize(Model* model, const Vector3& position);
 
@@ -17,4 +24,8 @@ private:
 	Model* model_ = nullptr;
 
 	uint32_t textureHandle_ = 0u;
+
+	//フェーズ
+	Phase phase = Phase::Approach;
+	
 };
